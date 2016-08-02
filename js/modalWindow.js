7f7f888184057491736c2dfe2940d2ca55638d79
hkalinouski <7742589@gmail.com>
2016-08-02 14:57:42 +0300

    animate

--- js/modalWindow.js
+++ js/modalWindow.js
@@ -2,10 +2,10 @@ var app = app || {};
 
 app.ModalWindow = function(elem) {
     this.elem = elem;
-    this.buttonEl = document.querySelector(elem.dataset.btn);
+    this.buttonEl = document.querySelector('.' + elem.dataset.btn);
     this._visible = false;
     this.animateDuration = 600;
-    this.timePassed = 0;
+    this.animateTimePassed = 0;
 };
 
 app.ModalWindow.prototype.init = function() {
@@ -19,32 +19,32 @@ app.ModalWindow.prototype.init = function() {
 
 app.ModalWindow.prototype.show = function (elem) {
     elem.style.display = 'block';
-    this.animate(elem, 'opacity', 0, 1, this.animateDuration);
+    this._visible = true;
+    this.animate(elem, 'opacity', 0, 1, this.animateDuration, function(){
+
+    });
+
 };
 
-app.ModalWindow.prototype.animate = function (elem, prop, start, finish, dur, callback) {
+app.ModalWindow.prototype.animate = function (elem, prop, start, end, dur, callback) {
     var startTime = Date.now();
-    var path = finish - start;
+    var path = end - start;
 
-
-    var timer = setInterval(function(){
+    var time = setInterval(function(){
         var t = (Date.now() - startTime) / dur;
 
-
-        if(t >= 1) {
-            this.timePassed = finish;
-            clearInterval(timer);
+        if(t >=1) {
+            this.animateTimePassed = end;
+            clearInterval(time);
             if(callback instanceof Function) {
                 callback();
             }
         } else {
-            this.timePassed = start + (t * path);
+            this.animateTimePassed = start + (t * path);
         }
 
+        elem.style[prop] = this.animateTimePassed;
+    }, 5);
 
-
-        elem.style[prop] = this.timePassed;
-
-    }.bind(this), 5);
 
 };
