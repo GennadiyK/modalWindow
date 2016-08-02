027f5e4b277d98c8b10d4a317b76805da72318f5
hkalinouski <7742589@gmail.com>
2016-08-02 18:04:38 +0300

    modal bg

--- js/modalWindow.js
+++ js/modalWindow.js
@@ -2,7 +2,10 @@ var app = app || {};
 
 app.ModalWindow = function(elem) {
     this.elem = elem;
-    this.buttonEl = document.querySelector('.' + elem.dataset.btn);
+    this.buttonShowEl = document.querySelector('.' + elem.dataset.btnShow);
+    this.buttonHideEl = document.querySelector('.' + elem.dataset.btnClose);
+    this.modalBg = document.createElement('div');
+    this.modalBg.classList.add('modal-overlay');
     this._visible = false;
     this.animateDuration = 600;
     this.animateTimePassed = 0;
@@ -10,20 +13,54 @@ app.ModalWindow = function(elem) {
 
 app.ModalWindow.prototype.init = function() {
     var self = this;
-    this.buttonEl.addEventListener('click', function(e){
+    this.buttonShowEl.addEventListener('click', function(e){
         e.preventDefault();
-        self.show(self.elem);
+        self.showElem(self.elem, self.modalBg);
     });
+
+    this.buttonHideEl.addEventListener('click', function(e){
+        e.preventDefault();
+        self.hideElem(self.elem, self.modalBg);
+
+    });
+};
+
+app.ModalWindow.prototype.showElem = function (elem, overlay) {
+
+    if(!this._visible) {
+        this._visible = true;
+        this.generateModalBg(overlay);
+        elem.style.display = 'block';
+        overlay.style.display = 'block';
+        this.animate(elem, 'opacity', 0, 1, this.animateDuration);
+        this.animate(overlay, 'opacity', 0, 1, this.animateDuration);
+    }
 
 };
 
-app.ModalWindow.prototype.show = function (elem) {
-    elem.style.display = 'block';
-    this._visible = true;
-    this.animate(elem, 'opacity', 0, 1, this.animateDuration, function(){
+app.ModalWindow.prototype.hideElem = function (elem, overlay) {
+    if(this._visible) {
+        this._visible = false;
+        this.animate(elem, 'opacity', 1, 0, this.animateDuration, function () {
 
-    });
+            elem.style.display = 'none';
+        });
+        this.animate(overlay, 'opacity', 1, 0, this.animateDuration, function(){
+            overlay.style.display = 'none';
+            this.removeModalBg(overlay);
+        }.bind(this));
+    }
+};
 
+app.ModalWindow.prototype.generateModalBg = function(elem) {
+    if(!document.querySelector('.modal-overlay')) {
+        document.querySelector('body').appendChild(elem);
+    }
+};
+
+app.ModalWindow.prototype.removeModalBg = function(elem) {
+    var parent = document.querySelector('body');
+    parent.removeChild(elem);
 };
 
 app.ModalWindow.prototype.animate = function (elem, prop, start, end, dur, callback) {
@@ -45,6 +82,5 @@ app.ModalWindow.prototype.animate = function (elem, prop, start, end, dur, callb
 
         elem.style[prop] = this.animateTimePassed;
     }, 5);
-
 
 };
